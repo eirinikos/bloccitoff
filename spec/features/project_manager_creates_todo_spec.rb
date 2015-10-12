bc8a13d2cf21b6664927f7156eaa9d40795c5528
Andrea <eirinikos@gmail.com>
2015-10-11 23:51:33 -0700

    completed TDD tutorial & created models/todo_spec.rb

---
+++ spec/features/project_manager_creates_todo_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'Project manager creates TODO' do
+  scenario '- successfully' do
+    visit(new_todo_path)
+    fill_in('Description', with: 'Meet up with the team')
+    click_button('Save')    
+      expect(page).to have_content('Your new TODO was saved.')
+      expect(page).to have_content('Meet up with the team')
+  end
+
+  scenario '- with description missing' do
+    
+    # expect user to see an error message
+
+  end
+end
---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Todo do
+  describe "validation methods" do
+
+    before do
+      @first_todo = Todo.new(description: "buy a milkshake")
+      @second_todo = Todo.new
+    end
+
+    describe '#valid?' do
+      it "requires the presence of a description" do
+        expect(@first_todo.save).to eq(true)
+        expect(@second_todo.save).to eq(false)
+        # expect TODO to not be saved
+      end   
+    end
+  end
+end
