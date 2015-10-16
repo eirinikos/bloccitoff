b49160727fb28e4a13fdfcbe9a8d2c5488d1e84e
Andrea <eirinikos@gmail.com>
2015-10-15 23:29:30 -0700

    implemented Action Mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,19 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      redirect_to root_url, :notice => "Signed up!"
-    else
-      render "new"
+
+    respond_to do |format|
+      if @user.save
+        # tell UserMailer to send a welcome email after save
+        UserMailer.welcome_email(@user).deliver_now
+        format.html { redirect_to(root_url, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+        # redirect_to root_url, :notice => "Signed up!"
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+        # render "new"
+      end
     end
   end
 
