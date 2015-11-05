6e66af06ec0df56eb850d42b16c8a765e99b5751
Andrea <eirinikos@gmail.com>
2015-11-05 13:46:30 -0800

    update User model to validate presence of password hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   before_update :encrypt_password
 
   validates_confirmation_of :password
-  validates_presence_of :password
+  validates_presence_of :password_hash
 
   validates_presence_of :email
   validates_uniqueness_of :email
